cd552347d99b360d359c17a27967b87fd53ac276
mttd <mttdmllr@gmail.com>
2016-08-01 13:46:11 -0700

    First Commit

---
+++ Fibonacci.sql
@@ -0,0 +1,11 @@
+WITH Fibonacci (PrevN, N) AS
+(
+     SELECT 0, 1
+     UNION ALL
+     SELECT N, PrevN + N
+     FROM Fibonacci
+     WHERE N < 1000000000
+)
+SELECT PrevN as Fibo
+     FROM Fibonacci
+     OPTION (MAXRECURSION 0);
---
+++ FizzBuzz.sql
@@ -0,0 +1,23 @@
+USE AdventureWorks2014
+GO
+
+    WITH CTE_Numbers 
+    AS 
+	(
+        SELECT 1 Number
+        UNION ALL
+        SELECT Number + 1
+          FROM CTE_Numbers
+         WHERE Number < 100
+    )
+
+    SELECT Number, 
+		   CASE	       
+	       WHEN Number % 3 = 0 AND Number % 5 = 0 THEN 'FizzBuzz'
+	       WHEN Number % 5 = 0 THEN 'Buzz'
+	       WHEN Number % 3 = 0 THEN 'Fizz'
+	       ELSE CAST(Number AS CHAR(10))
+	       END AS FizzBuzzTest 
+      FROM CTE_Numbers
+END
+GO
