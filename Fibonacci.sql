b2df8655c51f3376b2dc102e636a9502be8c364c
Matt Miller <mttdmllr@gmail.com>
2016-08-01 13:51:35 -0700

    Update Fib

--- Fibonacci.sql
+++ Fibonacci.sql
@@ -3,9 +3,9 @@ WITH Fibonacci (PrevN, N) AS
      SELECT 0, 1
      UNION ALL
      SELECT N, PrevN + N
-     FROM Fibonacci
-     WHERE N < 1000000000
+       FROM Fibonacci
+      WHERE N < 1000000000
 )
 SELECT PrevN as Fibo
-     FROM Fibonacci
-     OPTION (MAXRECURSION 0);
+  FROM Fibonacci
+OPTION (MAXRECURSION 0);
